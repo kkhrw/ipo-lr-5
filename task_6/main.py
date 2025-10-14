74c16dae513d239143e407a3b8bdc626561c357a
uljana.korolenko@gmail.com <uljana.korolenko@gmail.com>
2025-10-14 22:36:55 +0300

    Выполнила задания

---
+++ task_6/main.py
@@ -0,0 +1,7 @@
+with open ("text.txt", "r", encoding="utf-8") as file: #oткрываем файл
+    a = file.read() #cохраняем данные файла в переменной a
+    list = a.split() #cохраняем слова в список b
+    with open ("output.txt", "w", encoding="utf-8") as file1: #oткрываем файл
+        for i in list: #перебор списка
+            if len(i) > 5: #условие
+                file1.write(str(i)) #записываем элемент i в файл
