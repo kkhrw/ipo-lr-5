0b34a69f8e882c6dae832a82bead98f5208561a9
ульяна <uljana.korolenko@gmail.com>
2025-10-15 15:00:35 +0300

    задание6

--- task_6/main.py
+++ task_6/main.py
@@ -1,9 +1,9 @@
 #Короленко Ульяна
-#Вариант 2
-with open ("text.txt", "r", encoding="utf-8") as file: #oткрываем файл
-    a = file.read() #cохраняем данные файла в переменной a
-    list = a.split() #cохраняем слова в список b
-    with open ("output.txt", "w", encoding="utf-8") as file1: #oткрываем файл
-        for i in list: #перебор списка
-            if len(i) > 5: #условие
-                file1.write(str(i)) #записываем элемент i в файл
+#вариант 2
+filename = "text.txt" #создаем переменную и вписываем значение файла text.txt
+filename1 = "output.txt" #создаем переменную и вписываем значение файла output.txt
+with open(filename, "r", encoding="utf-8") as filein, open(filename1, "w", encoding="utf-8") as fileout: # блок кода открывает два файла: filein и fileout
+  for line in filein: #Этот цикл for перебирает по строкам файла filein
+     line = line.strip() #Удаляет пробелы в начале и в конце каждой строки
+     if len(line) > 5: #Проверяет, длиннее ли текущая строка line 5 символов
+        fileout.write(line + " ") #Если строка line длиннее 5 символов, то она записывается в файл fileout с добавлением пробела в конце.
